014cd3bf46653d32ef3ffa3ea3cbcb44ced6ca68
xiezhengyun <zhengyun.xie@wowjoy.cn>
2023-05-11 18:13:45 +0800

    feat .vitepress

---
+++ site/docs/.vitepress/config.js
@@ -0,0 +1,43 @@
+export default {
+  title: 'mydesignvue',
+  base: process.env.NODE_ENV === 'production' ? '/mydesignvue/' : '/',
+  themeConfig: {
+    nav: [
+      { text: '指南', link: '/guild/introduce' },
+      { text: '组件', link: '/components/button' }
+    ],
+    sidebar: {
+      '/guild/': [
+        {
+          text: '基础',
+          items: [
+            {
+              text: '介绍',
+              link: '/guild/introduce'
+            },
+            {
+              text: '快速开始',
+              link: '/guild/quickstart'
+            }
+          ]
+        }
+      ],
+      '/components/': [
+        {
+          text: '基础组件',
+          items: [
+            {
+              text: 'Button',
+              link: '/components/button'
+            },
+            {
+              text: 'Icon',
+              link: '/components/icon'
+            }
+          ]
+        }
+      ]
+    },
+    socialLinks: [{ icon: 'github', link: 'https://github.com/xiezhengyun/mydesignvue' }]
+  }
+}
